d59d071b7feb67d8e7cde7c80f8c0cd62fae097a
jeongsangtae <wjdtkdxo55@naver.com>
2022-10-15 20:16:12 +0900

    #11.3 Rooms

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,5 +1,13 @@
+from dataclasses import field
 from rest_framework.serializers import ModelSerializer
-from .models import Amenity
+from .models import Amenity, Room
+
+
+class RoomSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = "__all__"
+        depth = 1
 
 
 class AmenitySerializer(ModelSerializer):
