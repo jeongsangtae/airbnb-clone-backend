1da61050f3dc913513ff2958ed1356e04592a14c
jeongsangtae <wjdtkdxo55@naver.com>
2022-10-18 16:03:55 +0900

    #11.18 Reviews

--- rooms/views.py
+++ rooms/views.py
@@ -191,6 +191,9 @@ class RoomDetail(APIView):
 
 
 class RoomReviews(APIView):
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     def get_object(self, pk):
         try:
             return Room.objects.get(pk=pk)
@@ -212,6 +215,16 @@ class RoomReviews(APIView):
             many=True,
         )
         return Response(serializer.data)
+
+    def post(self, request, pk):
+        serializer = ReviewSerializer(data=request.data)
+        if serializer.is_valid():
+            review = serializer.save(
+                user=request.user,
+                room=self.get_object(pk),
+            )
+            serializer = ReviewSerializer(review)
+            return Response(serializer.data)
 
 
 class RoomAmenities(APIView):
