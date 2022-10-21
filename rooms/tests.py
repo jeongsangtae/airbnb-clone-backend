610e53411d7429a428dff7f381d1ae791f28fa63
jeongsangtae <wjdtkdxo55@naver.com>
2022-10-21 23:06:55 +0900

    #16.5 Test Authentication

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,6 +1,6 @@
-from urllib import response
 from rest_framework.test import APITestCase
 from . import models
+from users.models import User
 
 
 class TestAmenities(APITestCase):
@@ -165,3 +165,27 @@ class TestAmenity(APITestCase):
         response = self.client.delete("/api/v1/rooms/amenities/1")
 
         self.assertEqual(response.status_code, 204)
+
+
+class TestRoom(APITestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username="test",
+        )
+        user.set_password("123")
+        user.save()
+        self.user = user
+
+    def test_create_room(self):
+
+        response = self.client.post("/api/v1/rooms/")
+
+        self.assertEqual(response.status_code, 403)
+
+        self.client.force_login(
+            self.user,
+        )
+
+        response = self.client.post("/api/v1/rooms/")
+
+        print(response.json())
