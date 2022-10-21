4c461e7a815a2e95a6dde7b737e0e38f192bdfaa
jeongsangtae <wjdtkdxo55@naver.com>
2022-10-21 22:40:56 +0900

    #16.4 Amenity Detail Test + Challenge

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,3 +1,4 @@
+from urllib import response
 from rest_framework.test import APITestCase
 from . import models
 
@@ -87,8 +88,77 @@ class TestAmenity(APITestCase):
             description=self.DESC,
         )
 
+    def test_amenity_not_found(self):
+
+        response = self.client.get("/api/v1/rooms/amenities/2")
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_amenity(self):
+
+        response = self.client.get("/api/v1/rooms/amenities/1")
+
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+
+        self.assertEqual(
+            data["name"],
+            self.NAME,
+        )
+        self.assertEqual(
+            data["description"],
+            self.DESC,
+        )
+
     def test_put_amenity(self):
-        pass
+
+        new_name = "new name"
+        new_desc = "new desc"
+
+        response = self.client.put("/api/v1/rooms/amenities/1", data={"name": new_name})
+
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+
+        self.assertEqual(data["name"], new_name)
+        self.assertEqual(data["description"], self.DESC)
+
+        response = self.client.put(
+            "/api/v1/rooms/amenities/1", data={"description": new_desc}
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+
+        self.assertEqual(
+            data["name"],
+            new_name,
+        )
+        self.assertEqual(
+            data["description"],
+            new_desc,
+        )
+
+        new_name2 = "new name2"
+        new_desc2 = "new desc2"
+
+        response = self.client.put(
+            "/api/v1/rooms/amenities/1",
+            data={
+                "name": new_name2,
+                "description": new_desc2,
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+
+        self.assertEqual(data["name"], new_name2)
+        self.assertEqual(data["description"], new_desc2)
 
     def test_delete_amenity(self):
 
